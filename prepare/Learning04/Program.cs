77fb1b2c3c05115ca88c2202440e4e6d2887d735
Youan22 <you18034@byui.edu>
2023-02-25 21:19:37 -0700

    mod

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        string summary = assignment.GetSummary();
+
+        Console.WriteLine(summary);
+
+        MathAssignment mathAssignment = new MathAssignment("Section", "Fractions", "7.3", "8-19");
+        //string summary = mathAssignment.GetSummary();
+        string homeworkList = mathAssignment.GetHomeworkList();
+        //Console.WriteLine(summary);
+        Console.WriteLine(homeworkList);
+
+
     }
 }
---
+++ prove/Develop02/entry.cs
@@ -0,0 +1,32 @@
+class Entry
+{
+    public string Prompt { get; }
+    public string Response { get; }
+    public string Date { get; }
+
+    public Entry(
+
+using System;
+
+class Entry
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public Entry()
+    {
+        Console.WriteLine("Enter title: ");
+        Title = Console.ReadLine();
+        Console.WriteLine("Enter content: ");
+        Content = Console.ReadLine();
+        CreatedAt = DateTime.Now;
+    }
+
+public Entry(string prompt, string response, DateTime date)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
+}
