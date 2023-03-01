8fb34e4a7275512d6a2f8e333c7233d36d529803
Youan22 <you18034@byui.edu>
2023-03-01 12:49:51 -0700

    programstarter

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+
+        for (int i = 5; i > 0; i--)
+        {
+
+        Console.WriteLine(i);
+        Thread.Sleep(1000);
+        }
+        // Console.WriteLine(4);
+        // Thread.Sleep(1000);
+        // Console.WriteLine(3);
+        // Thread.Sleep(1000);
+        // Console.WriteLine(2);
+        // Thread.Sleep(1000);
+        // Console.WriteLine(1);
+        // Thread.Sleep(1000);
+        Console.WriteLine("Done");
     }
 }
