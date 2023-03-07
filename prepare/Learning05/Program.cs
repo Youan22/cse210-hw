c0a87f5c02bea4ecd1ff390a55873d55e6bad938
Youan22 <you18034@byui.edu>
2023-03-07 12:09:30 -0700

    prepare

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 5));
+        shapes.Add(new Rectangle("green", 4, 6));
+        shapes.Add(new Circle("blue", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine("Color: {0}", shape.Color);
+            Console.WriteLine("Area: {0}\n", shape.GetArea());
+        }
     }
 }
