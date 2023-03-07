6664f6a395a9a6bc567c910a19eeac2c30075df2
Youan22 <you18034@byui.edu>
2023-03-07 13:08:57 -0700

    cc

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Create a new scripture
+        Scripture scripture = new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+
+        // Display the scripture
+        Console.WriteLine(scripture.GetRenderedText());
+        Console.WriteLine("Press ENTER to continue or type 'quit' to exit.");
+
+        // Hide words until the scripture is completely hidden
+        while (!scripture.IsCompletelyHidden())
+        {
+            string input = Console.ReadLine();
+            if (input == "quit")
+            {
+                break;
+            }
+            scripture.HideWords();
+            Console.WriteLine(scripture.GetRenderedText());
+        }
     }
 }
+
