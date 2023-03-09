1999a8253d1403cdbbddc2fbd3ac0c7163de7178
Youan22 <you18034@byui.edu>
2023-03-09 07:47:34 -0700

    todayupdates

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,14 +1,25 @@
+using System;
+using System.Globalization;
+
 public class Entry
 {
     private string prompt;
     private string response;
-    private string date;
+    private DateTime date;
+    private string v;
 
-    public Entry(string prompt, string response, string date)
+    public Entry(string prompt, string response, DateTime date)
     {
         this.prompt = prompt;
         this.response = response;
         this.date = date;
+    }
+
+    public Entry(string prompt, string response, string v)
+    {
+        this.prompt = prompt;
+        this.response = response;
+        this.v = v;
     }
 
     public string Prompt
@@ -23,19 +34,23 @@ public class Entry
         set { response = value; }
     }
 
-    public string Date
+    public DateTime Date
     {
         get { return date; }
         set { date = value; }
     }
 
+
     public override string ToString()
     {
-        return $"Prompt: {prompt}\nResponse: {response}\nDate: {date}\n";
+        return $"Prompt: {prompt}\nResponse: {response}\nDate: {date.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)}\n";
     }
 
-    internal ReadOnlySpan<char> ToCSVString()
-    {
-        throw new NotImplementedException();
-    }
+internal ReadOnlySpan<char> ToCSVString()
+{
+        string Text = null;
+        string[] fields = { date.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture), Text };
+    return string.Join(",", fields).AsSpan();
+}
+
 }
